e477c2cc257df5821c99036221da8f1f4a974828
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2021-05-03 15:52:23 -0700

    some stuff

--- lab2/lab2.php
+++ lab2/lab2.php
@@ -17,30 +17,23 @@
 </head>
 
 <body>
-<div class="row">
-    <div class="col-md-6 mx-auto p-0">
-        <div class="card">
-            <div class="login-box">
-                <div class="login-snip"> <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Login</label> <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
-                    <div class="login-space">
-                        <div class="login">
-                            <div class="group"> <label for="user" class="label">Username</label> <input id="user" type="text" class="input" placeholder="Enter your username"> </div>
-                            <div class="group"> <label for="pass" class="label">Password</label> <input id="pass" type="password" class="input" data-type="password" placeholder="Enter your password"> </div>
-                            <div class="group"> <input id="check" type="checkbox" class="check" checked> <label for="check"><span class="icon"></span> Keep me Signed in</label> </div>
-                            <div class="group"> <input type="submit" class="button" value="Sign In"> </div>
-                        </div>
-                        <div class="sign-up-form">
-                            <div class="group"> <label for="user" class="label">Username</label> <input id="user" type="text" class="input" placeholder="Create your Username"> </div>
-                            <div class="group"> <label for="pass" class="label">Password</label> <input id="pass" type="password" class="input" data-type="password" placeholder="Create your password"> </div>
-                            <div class="group"> <label for="pass" class="label">Repeat Password</label> <input id="pass" type="password" class="input" data-type="password" placeholder="Repeat your password"> </div>
-                            <div class="group"> <input type="submit" class="button" value="Sign Up"> </div>
-                        </div>
-                    </div>
-                </div>
+    <div class="login">
+        <div id="login">
+            <a><h1>Sign In:</h1></a>
+            <div class="form-group">
+                <label for="username-login">Username</label>
+                <input type="text" class="form-control" id="username-login" placeholder="Enter Username">
+            </div>
+            <div class="form-group">
+                <label for="password-login">Password</label>
+                <input type="password" class="form-control" id="password-login" placeholder="Enter Password">
+            </div>
+            <div class="d-flex">
+                <button type="submit" id="sign-in" class="m-1 btn btn-light">Sign in</button>
+                <button type="submit" id="sign-up" class="m-1 btn btn-light d-none">Sign up</button>
             </div>
         </div>
     </div>
-</div>
 </body>
 
 </html>
--- lab2/login.php
+++ lab2/login.php
@@ -1,29 +1,40 @@
 <?php
 include_once 'mysql.php';
 
-function password_verify_with_rehash($username,$password, $hash) {
+function password_verify_with_rehash($username, $password, $hash)
+{
     if (!password_verify($password, $hash)) {
         return false;
     }
 
     if (password_needs_rehash($hash, PASSWORD_DEFAULT)) {
         $hash = password_hash($password, PASSWORD_DEFAULT);
-        $user=array("password"=>$hash);
-        update_user($username,$user);
+        $user = array("password" => $hash);
+        update_user($username, $user);
     }
 
     return true;
 }
 
-function create_user($username,$password){
+function create_user($username, $password)
+{
     $hash = password_hash($password, PASSWORD_DEFAULT);
-    $user=array("username"=>$username,"password"=>$hash);
-    $result=insert($user);
+    $user = array("username" => $username, "password" => $hash);
+    $result = insert($user);
     close_connection();
-    if(!$result){
+    if (!$result) {
         return false;
     }
     return true;
 }
+// Takes raw data from the request
+$json = file_get_contents('php://input');
+// Converts it into a PHP object
+$data = json_decode($json);
+if (json_last_error() != JSON_ERROR_NONE) {
+    die("invalid request2");
+}
+var_dump($data);
+
 
 // create_user("test","password");
